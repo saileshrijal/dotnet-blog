6b3ea02b3bf84bc72005cb71979959d93afdf764
saileshrijal <saileshrijal1@gmail.com>
2022-05-28 15:32:23 +0545

    First Commit

---
+++ AliveBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AliveBlog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AliveBlog.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ AliveBlog/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AliveBlog.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
