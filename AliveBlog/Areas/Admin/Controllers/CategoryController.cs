925c6551f16a41cb65bdafc04f8cdeae28ebf1c0
saileshrijal <saileshrijal1@gmail.com>
2022-05-29 08:38:08 +0545

    admin template added

---
+++ AliveBlog/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AliveBlog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+
+    }
+}
--- AliveBlog/Program.cs
+++ AliveBlog/Program.cs
@@ -34,6 +34,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "Area",
+    pattern: "{area=exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
